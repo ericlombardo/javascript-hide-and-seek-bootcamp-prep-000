6099b885754da9c262c690ab20146cb63cadace3
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-01 18:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function nestedTarget() {
 // returns the deepest nested child in #grand-node
 function deepestChild() {
   return document.getElementById('grand-node').querySelector(div);
+  console.log("hello");
 }
 
 // increases ranks in .ranked-list by n
