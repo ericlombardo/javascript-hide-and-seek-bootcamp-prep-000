7692b2fcae9cb3c516ddba29fa13cbb605a22217
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-01 20:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deepestChild() {
 
 // increases ranks in .ranked-list by n
 function increaseRankBy(n) {
-  var rankedLists = document.getElementById('app').querySelectAll('ul.ranked-list li');
+  var rankedLists = document.querySelectAll('ul.ranked-list li');
   for (let i = 0; i < rankedLists.length; i++) {
     rankedLists[i] = parseInt(rankedLists[i]) + 1;
   }
